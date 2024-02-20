237131a69afb54e33f1469d54360b570ea4aefcd
Satria Abrar <satria.abrarr@gmail.com>
2024-02-20 12:14:32 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,46 @@ Route::get('/user/{name?}', function ($name=null) {
 Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya '.$name;
     });
+
+// route name
+Route::get('/user/profile', function() {
+    //
+   })->name('profile');
+
+// Route Group dan Route Prefixes
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+        // Uses first & second middleware...
+    });
+
+    Route::get('/user/profile', function () {
+        // Uses first & second middleware...
+    });
+});
+
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
+
+// Route Prefixes
+Route::prefix('admin')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+    });
+
+// Redirect Routes
+Route::redirect('/here', '/there');
+
+// View Routes
+Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
