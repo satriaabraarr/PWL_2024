4c2f3e6530411a801ec863173efabd0dff8c311c
Satria Abrar <satria.abrarr@gmail.com>
2024-02-20 11:45:41 +0700

    jobsheet

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,40 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/world', function () {
     return 'World';
-   });
-   
+   });   
 
 Route::get('/hello', function () {
     return 'Hello World';
    });
-   
+
+Route::get('/welcome', function () {
+    return 'Selamat Datang';
+   });
+
+Route::get('/about', function () {
+    return '2241720260 <br>Satria Abrar';
+   });
+
+// route parameters
+
+Route::get('/user/{name}', function ($name) {
+return 'Nama Saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+ return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID {{$id}}";
+});
+
+// optional parameters
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+    });
